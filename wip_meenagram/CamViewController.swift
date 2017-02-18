6e3d37c283270e1785c5d15961643850ac664db8
aburhan <ameenah.burhan@gmail.com>
2017-02-17 20:54:24 -0500

    Wiring up the User Interface

---
+++ wip_meenagram/CamViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CamViewController.swift
+//  wip_meenagram
+//
+//
+
+import UIKit
+
+class CamViewController: UIViewController {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var captionTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sharePhoto(_ sender: Any) {
+    }
+    @IBAction func cancelUpload(_ sender: Any) {
+    }
+    @IBAction func pickPhotoFromGallery(_ sender: Any) {
+    }
+    @IBAction func takePhotoAction(_ sender: Any) {
+    }
+}
